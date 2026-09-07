f086c6076d2534ffeb169b070b84cfec91163238
evanlambb <evanlamb848@gmail.com>
2026-09-06 22:37:58 -0400

    basic classes outlined

---
+++ snake.py
@@ -0,0 +1,16 @@
+from typing import List
+import pygame
+
+
+# This object manages the game state, storing things like the board and the snake
+# and exposing methods like step(action), reset(), and get_state()
+class GameState:
+  pass
+
+# This is the ai class that plays the game
+class Agent:
+  pass
+
+# This object controls the main game loop and rendering
+class GameController:
+  pass
