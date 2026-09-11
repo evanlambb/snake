5f7048379d17dea2ed4a41d08636359be18947bb
evanlambb <evanlamb848@gmail.com>
2026-09-10 21:37:24 -0400

    beginning to outline the different features of each class

--- snake.py
+++ snake.py
@@ -5,7 +5,21 @@ import pygame
 # This object manages the game state, storing things like the board and the snake
 # and exposing methods like step(action), reset(), and get_state()
 class GameState:
-  pass
+  def __init__(self, rows, columns):
+    self.rows = rows
+    self.columns = columns
+    self.snake = []
+    self.board = []
+    self.head = -1 # TODO
+    self.tail = -1 # TODO
+    self.score = 1
+
+  # perform a single move of the snake
+  def step(self, action):
+    pass
+  # reset the board to a default board
+  def reset(self):
+    pass
 
 # This is the ai class that plays the game
 class Agent:
